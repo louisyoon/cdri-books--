e51bb7c5c5cdc8ed461c0c709f853cb048d41c76
louisyoon <louisyoon23@gmail.com>
2026-04-03 09:54:48 +0900

    add: 스크롤 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Header from "@/components/Header"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Section1 from "./components/Main/Section1";
 import Section2 from "./components/Main/Section2";
 
 function App() {
   const [tab, setTab] = useState<number>(0);
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [tab])
+
   const TabSection = tab === 0 ? <Section1 /> : <Section2 />
 
   return (
